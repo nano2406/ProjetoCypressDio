ea2025b5838dfd6464340414e31fe98e997e4588
Luciano Filho <luciano.costa.filho92@gmail.com>
2021-04-10 19:20:12 -0300

    Finalização do projeto CyPress Dio

---
+++ cypress/integration/logout.spec.js
@@ -0,0 +1,11 @@
+
+
+describe('Logut', () =>{
+    it('Logout via Perfil', () => {
+        cy.login('testecypress@testecypress.com', 'testecypress')
+        cy.contains('Settings').click()
+        cy.url().should('include', '/settings')
+        cy.get('.btn-outline-danger').click()
+        
+    })
+})
